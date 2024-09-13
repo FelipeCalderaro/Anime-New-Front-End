dd55c15e2de8b2c780ae38dcb9b1b1882f48e96e
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2024-09-13 14:23:40 -0300

    Added to the home page the TV Shorts, Movies, and specials (#19)
    * Added to the home page the TV Shorts, Movies, and specials
    
    * Added leftovers to the home page

---
+++ components/adContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="justify-center w-full h-[90px] flex my-8">
+    <div class="w-[720px] h-[90px] bg-indigo-500 mx-5">ANUNCIO</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
--- utils/season_handlers.ts
+++ utils/season_handlers.ts
@@ -8,9 +8,9 @@ export interface SeasonsRowSelector {
 
 const seasons: string[] = [
     "Inverno",
-    "Outono",
-    "Verão",
     "Primavera",
+    "Verão",
+    "Outono",
 ];
 
 export function getYearBySeason(): SeasonsRowSelector[] {
@@ -22,23 +22,23 @@ export function getYearBySeason(): SeasonsRowSelector[] {
         case MediaSeason.SUMMER:
             return [
                 { seasonName: seasons[0], seasonEnum: MediaSeason.WINTER, seasonYear: year + 1 },
-                { seasonName: seasons[1], seasonEnum: MediaSeason.FALL, seasonYear: year },
+                { seasonName: seasons[1], seasonEnum: MediaSeason.SPRING, seasonYear: year },
                 { seasonName: seasons[2], seasonEnum: MediaSeason.SUMMER, seasonYear: year },
-                { seasonName: seasons[3], seasonEnum: MediaSeason.SPRING, seasonYear: year },
+                { seasonName: seasons[3], seasonEnum: MediaSeason.FALL, seasonYear: year },
             ]
         case MediaSeason.FALL:
             return [
                 { seasonName: seasons[0], seasonEnum: MediaSeason.WINTER, seasonYear: year + 1 },
-                { seasonName: seasons[1], seasonEnum: MediaSeason.FALL, seasonYear: year + 1 },
+                { seasonName: seasons[1], seasonEnum: MediaSeason.SPRING, seasonYear: year + 1 },
                 { seasonName: seasons[2], seasonEnum: MediaSeason.SUMMER, seasonYear: year },
-                { seasonName: seasons[3], seasonEnum: MediaSeason.SPRING, seasonYear: year }
+                { seasonName: seasons[3], seasonEnum: MediaSeason.FALL, seasonYear: year }
             ];
         default:
             return [
                 { seasonName: seasons[0], seasonEnum: MediaSeason.WINTER, seasonYear: year },
-                { seasonName: seasons[1], seasonEnum: MediaSeason.FALL, seasonYear: year },
+                { seasonName: seasons[1], seasonEnum: MediaSeason.SPRING, seasonYear: year },
                 { seasonName: seasons[2], seasonEnum: MediaSeason.SUMMER, seasonYear: year },
-                { seasonName: seasons[3], seasonEnum: MediaSeason.SPRING, seasonYear: year }
+                { seasonName: seasons[3], seasonEnum: MediaSeason.FALL, seasonYear: year }
             ];
     }
 }
