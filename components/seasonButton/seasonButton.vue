f51b2b5508a5411b3dd207170b12e16b13dbdd29
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2024-09-16 13:32:16 -0300

    Updated icon imports

--- components/seasonButton/seasonButton.vue
+++ components/seasonButton/seasonButton.vue
@@ -1,8 +1,14 @@
 <template>
-  <q-btn flat @click="onClick" class="w-[130px] py-1 px-2 text-sm font-normal leading-5 cursor-pointer" :class="selected
-    ? 'bg-primary-01'
-    : 'bg-card-component hover:bg-gradient-to-t  hover:from-primary-01 hover:to-65% hover:to-ui-component2 transition duration-300 ease-in-out'
-    ">
+  <q-btn
+    flat
+    @click="onClick"
+    class="w-[130px] py-1 px-2 text-sm font-normal leading-5 cursor-pointer hover:-translate-y-3 transition delay-150 duration-300 ease-in-out"
+    :class="
+      selected
+        ? 'bg-primary-01'
+        : 'bg-card-component hover:bg-gradient-to-t  hover:from-primary-01 hover:to-65% hover:to-ui-component2 transition duration-300 ease-in-out'
+    "
+  >
     <div class="flex flex-col items-center">
       <img class="w-7 h-7 mb-1" :src="icon" alt="Season Icon" />
       {{ label }}
@@ -19,7 +25,6 @@ interface SeasonButtonProps {
 }
 
 const props = defineProps<SeasonButtonProps>();
-
 </script>
 
 <style></style>
