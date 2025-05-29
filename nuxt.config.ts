ea2886d5eaa0c2828cfd9e15373a2678e1c5c457
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2025-05-29 19:52:19 -0300

    Another try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,6 @@ export default defineNuxtConfig({
 
   app: {
     baseURL: "/temporadas/",
-    buildAssetsDir: "/_nuxt/",
     head: {
       title: APP_CONFIGS.title,
       titleTemplate: "%s",
